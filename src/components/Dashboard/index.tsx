d24e7789f27e92f92dcc177e1e8ac11532da85fc
iki789 <ikhlak789@gmail.com>
2019-07-25 04:14:25 +0530

    added dashboard structure

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import { Redirect, RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps } from 'react-router-dom';
+import { Container, AppBar, Toolbar, Typography, withStyles  } from '@material-ui/core';
 import { Auth } from '../../lib/auth';
 
-class Dashboard extends Component<RouteComponentProps> {
+
+
+class Dashboard extends Component<IDashboard & RouteComponentProps> {
+  
   componentDidMount(){
     if(!Auth.isLoggedIn){
       this.props.history.push('/login')
@@ -10,11 +14,32 @@ class Dashboard extends Component<RouteComponentProps> {
   }
   render() {
     return (
-      <div>
-        You are logged in!
-      </div>
+      <React.Fragment>
+        <AppBar position="static">
+        <Toolbar>
+          <Typography>CCG: Dashboard</Typography>
+        </Toolbar>
+        </AppBar>
+        <Container className={this.props.classes.container}>
+          <Typography variant="h4">
+            Welcome <Typography display="inline" variant="h6">Dr. {Auth && Auth.user ? Auth.user.name : '' }</Typography> 
+          </Typography>
+        </Container >
+      </React.Fragment>
     )
   }
 }
 
-export default Dashboard;
+interface IDashboard{
+  classes: any
+}
+
+let styles = () => {
+  return {
+    container: {
+      marginTop: '1rem'
+    }
+  }
+}
+
+export default withStyles(styles)(Dashboard);
