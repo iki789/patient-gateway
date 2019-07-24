850756d0ad82c804c253f75144c72ba47d6115e2
iki789 <ikhlak789@gmail.com>
2019-07-24 10:37:24 +0530

    initalized routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React, { lazy, Suspense } from 'react';
-import { Grid } from '@material-ui/core'
+import { Grid } from '@material-ui/core';
+import { BrowserRouter, Route } from 'react-router-dom';
 const Auth = lazy(() => import('./components/Auth/index'));
 
 const App: React.FC = () => {
   return (
-    <Grid container>
-      <Suspense fallback={'Loading...'}>
-        <Grid item xs={12}>
-          <Auth />
-        </Grid>
-      </Suspense>
-    </Grid>
+    <BrowserRouter>
+      <Grid container>
+        <Suspense fallback={'Loading...'}>
+          <Route exact path='/' component={()=><div>Welcome</div>} />
+          <Route exact path='/login' component={Auth} />
+        </Suspense>
+      </Grid>
+    </BrowserRouter>
   );
 }
 
