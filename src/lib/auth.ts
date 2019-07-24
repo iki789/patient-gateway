89809e9698654a97c742709ad351319ea96dda85
iki789 <ikhlak789@gmail.com>
2019-07-24 22:39:01 +0530

    implemented all methods

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,32 +1,55 @@
+import jwt from 'jwt-simple';
 import { Users, IUser } from '../db';
 
 export class Auth implements IAuth{
-  
   isLoggedIn: boolean = false ;
   
-  authenticate(username: string, password: string){
+  constructor(){
+    this.isLoggedIn = this.loggedIn();
+  }
+
+  authenticate(username: string, password: string): boolean{
     let user: IUser | undefined = Users.find(user=>user.username === username);
     if(user){
       if(user.password === password){
-        this.login(user.username);
+        this.login(user);
+        this.isLoggedIn = true;
         return true;
       }
     }
     return false;
   }
 
-  login(username: string){
+  private generateToken(user: IUser): string{
+    let secret = "The lost castle";
+    let payload = {
+      sub: user.id, 
+      name: `${user.firstname} ${user.middleInitial.length ? user.middleInitial + '.' : ''} ${user.lastname}`,
+      exp: Math.floor(Date.now() / 1000) + (60 * 60)
+    };
+    let token: string = jwt.encode(payload,secret);
+    return token;
   }
 
-  logout(){
-
+  login(user: IUser): IUser{
+    localStorage.setItem('token', this.generateToken(user))
+    return user;
   }
 
+  logout(): void{
+    localStorage.removeItem('token');
+  }
+
+  private loggedIn():boolean{
+    let isLoggedIn = localStorage.getItem('token') ? true : false ;
+    this.isLoggedIn = isLoggedIn;
+    return this.isLoggedIn;
+  }
 }
 
 interface IAuth{
   authenticate: (username: string, password: string)=>boolean
-  login: (username:string)=>void
+  login: (user: IUser)=>IUser
   logout: ()=>void
   isLoggedIn: boolean
 }
