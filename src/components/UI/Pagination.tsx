4ee43f898301293a7ec4ab28d619c7faafaf4140
iki789 <ikhlak789@gmail.com>
2019-07-26 17:38:23 +0530

    added styled buttons

--- src/components/UI/Pagination.tsx
+++ src/components/UI/Pagination.tsx
@@ -1,22 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, createStyles, makeStyles, Theme } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>{
+  return createStyles({
+    pagination:{
+      marginTop: theme.spacing(3)
+    },
+    button: {
+      padding: theme.spacing(1),
+      borderRadius: '50%',
+      minWidth: '42px',
+    },
+    active:{
+      backgroundColor: theme.palette.primary.light,
+      color: '#fff !important'
+    },
+    input: {
+      display: 'none',
+    }
+  })
+}
+);
 
 export const Pagination: React.FC<PaginationProps> = (props:PaginationProps) => {
+  const classes:any = useStyles();
   const pages: Array<number> = [];
   for(let i = 1; i <= props.pageCount; i++){
     pages.push(i);
   }
+  const [currentPage, setCurrentPage] = useState(1);
 
   const changePage = (e: React.MouseEvent<HTMLElement>) =>{
-    return props.handlePageChange(+(e.currentTarget.innerText))
+    setCurrentPage(+(e.currentTarget.innerText));
+    return props.handlePageChange(+(e.currentTarget.innerText));
   }
 
   return (
-    <div className="pagination">
-      {pages.length > 1 ? pages.map(p=><p key={p} onClick={changePage}>{p}</p>) : null }      
+    <div className={classes.pagination}>
+      {
+        pages.length > 1 ? 
+          pages.map(p=>
+            <Button 
+              key={p} 
+              className={`${classes.button} ${p === currentPage ? classes.active : null}`} 
+              onClick={changePage} 
+              disabled={currentPage === p}>
+                {p}
+              </Button>
+          ) : 
+        null 
+      }      
     </div>
   )
 }
-
 
 interface PaginationProps{
   pageCount: number;
