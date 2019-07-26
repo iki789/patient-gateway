3454da43a2f862c2ba4dff7ab5a11d47350a5b34
iki789 <ikhlak789@gmail.com>
2019-07-27 01:25:21 +0530

    added samples class

---
+++ src/lib/sample.ts
@@ -0,0 +1,32 @@
+import { Pagination } from './pagination';
+import { Samples as SamplesDB, ISample } from '../db/index';
+
+export class Sample extends Pagination implements IBase {
+	samples: ISample[] = SamplesDB;
+
+	constructor(samples: ISample[] = []) {
+		super(samples);
+		this.items = this.samples;
+	}
+
+	getById(id: number) {
+		return this.samples.filter((s) => s.id === id)[0];
+	}
+
+	getAll(page: number) {
+		page = page < 1 ? 1 : page;
+		this.items = this.samples;
+		return this.getItemsOnPage(page);
+	}
+
+	getByPatient(patientId: number, page: number) {
+		this.items = this.samples.filter((s) => s.patientId === patientId);
+		return this.getItemsOnPage(page);
+	}
+}
+
+interface IBase {
+	getById: (id: number) => ISample;
+	getByPatient: (id: number, page: number) => ISample[];
+	getAll: (page: number) => ISample[];
+}
