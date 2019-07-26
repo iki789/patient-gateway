cffb7a3c614cd109e715291d0ce8bc709ff7deb6
iki789 <ikhlak789@gmail.com>
2019-07-26 23:21:34 +0530

    added new repeative methods

--- src/lib/pagination.ts
+++ src/lib/pagination.ts
@@ -1,38 +1,48 @@
-export class Pagination implements IPagination{
-  items: Array<any> = []
-  pages: number = 0;
-  perPage: number;
+export class Pagination implements IPagination {
+	items: Array<any> = [];
+	pages: number = 0;
+	perPage: number;
 
-  constructor(items: Array<any>, perPage: number = 25){
-    this.items = items;
-    this.sortItems();
-    this.perPage = perPage;
-    this.calculatePages();
-  }
+	constructor(items: Array<any>, perPage: number = 25) {
+		this.items = items;
+		this.sortItems();
+		this.perPage = perPage;
+		this.calculatePages();
+	}
 
-  calculatePages(): void{
-    this.pages = Math.ceil((this.items.length/this.perPage))
-  }
+	calculatePages(): void {
+		this.pages = Math.ceil(this.items.length / this.perPage);
+	}
 
-  sortItems(): void{
-    this.items.sort((a,b)=>a-b);
-  }
+	sortItems(): void {
+		this.items.sort((a, b) => a - b);
+	}
 
-  getItemsOnPage(page: number): Array<any>{
-    let skip = (page*this.perPage) - this.perPage;
-    let pageItems: Array<any> = [];
-    for(let i = 0; i < this.perPage; i++){
-      if(!this.items[skip+i]){
-        break;
-      }
-      pageItems.push(this.items[skip+i]);
-    }
-    return pageItems;
-  }
+	getItemsOnPage(page: number): Array<any> {
+		let skip = page * this.perPage - this.perPage;
+		let pageItems: Array<any> = [];
+		for (let i = 0; i < this.perPage; i++) {
+			if (!this.items[skip + i]) {
+				break;
+			}
+			pageItems.push(this.items[skip + i]);
+		}
+		return pageItems;
+	}
+
+	set setPerPage(perPage: number) {
+		this.perPage = perPage;
+	}
+
+	get pageCount(): number {
+		this.calculatePages();
+		return this.pages;
+	}
 }
 
-export interface IPagination{
-  pages: number;
-  perPage: number;
-  getItemsOnPage: (page: number) => Array<any>;
+export interface IPagination {
+	pages: number;
+	perPage: number;
+	calculatePages: () => void;
+	getItemsOnPage: (page: number) => Array<any>;
 }
