1b32d961cf68dfc240ba5479c16db0b7bb77bc60
iki789 <ikhlak789@gmail.com>
2019-07-26 17:46:18 +0530

    implemented patient class

---
+++ src/lib/patient.ts
@@ -0,0 +1,40 @@
+import { Pagination, IPagination } from './pagination';
+import { Patients as PatientsDB, IPatient } from '../db/index';
+
+export class Patient implements IBase{
+  userId: number;
+  patients: IPatient[] = [];
+  perPage = 5;
+  paginationHelper: IPagination;
+
+  constructor(userId: number){
+    this.userId = userId;
+    this.patients = PatientsDB.filter(p=>p.userId === this.userId);
+    this.paginationHelper = new Pagination(this.patients, this.perPage);
+  }
+
+  getById(id:number){
+    return this.patients.filter(p=>p.id === id)[0];
+  }
+
+  getAll(page:number){
+    page = page < 1 ? 1 : page;
+    return this.paginationHelper.getItemsOnPage(page);
+  }
+
+  get pageCount(){
+    return this.paginationHelper.pages;
+  }
+
+  getAge(birthday: string):number{
+    return ((new Date()).getFullYear()) - ((new Date(birthday).getFullYear()));
+  }
+}
+
+interface IBase{
+  perPage: number;
+  paginationHelper: IPagination;
+  pageCount: number
+  getById: (id: number)=>IPatient;
+  getAll: (page: number)=>Array<IPatient>;
+}
