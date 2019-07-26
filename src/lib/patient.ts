6681303f071c9780bd6a5252e55b1dda5116ffc3
iki789 <ikhlak789@gmail.com>
2019-07-26 23:22:42 +0530

    extend pagination class

--- src/lib/patient.ts
+++ src/lib/patient.ts
@@ -1,44 +1,32 @@
-import { Pagination, IPagination } from './pagination';
+import { Pagination } from './pagination';
 import { Patients as PatientsDB, IPatient } from '../db/index';
 
-export class Patient implements IBase{
-  userId: number;
-  patients: IPatient[] = [];
-  perPage = 5;
-  paginationHelper: IPagination;
+export class Patient extends Pagination implements IBase {
+	userId: number;
+	patients: IPatient[] = [];
 
-  constructor(userId: number){
-    this.userId = userId;
-    this.patients = PatientsDB.filter(p=>p.userId === this.userId);
-    this.paginationHelper = new Pagination(this.patients, this.perPage);
-  }
+	constructor(userId: number, patients: IPatient[] = []) {
+		super(patients);
+		this.userId = userId;
+		this.patients = PatientsDB.filter((p) => p.userId === this.userId);
+		this.items = this.patients;
+	}
 
-  getById(id:number){
-    return this.patients.filter(p=>p.id === id)[0];
-  }
+	getById(id: number) {
+		return this.patients.filter((p) => p.id === id)[0];
+	}
 
-  getAll(page:number){
-    page = page < 1 ? 1 : page;
-    return this.paginationHelper.getItemsOnPage(page);
-  }
+	getAll(page: number) {
+		page = page < 1 ? 1 : page;
+		return this.getItemsOnPage(page);
+	}
 
-  set setPerPage(perPage: number){
-    this.perPage = perPage;
-    this.paginationHelper.perPage = perPage;
-  }
-  get pageCount(){
-    return this.paginationHelper.pages;
-  }
-
-  getAge(birthday: string):number{
-    return ((new Date()).getFullYear()) - ((new Date(birthday).getFullYear()));
-  }
+	getAge(birthday: string): number {
+		return new Date().getFullYear() - new Date(birthday).getFullYear();
+	}
 }
 
-interface IBase{
-  perPage: number;
-  paginationHelper: IPagination;
-  pageCount: number
-  getById: (id: number)=>IPatient;
-  getAll: (page: number)=>Array<IPatient>;
+interface IBase {
+	getById: (id: number) => IPatient;
+	getAll: (page: number) => Array<IPatient>;
 }
