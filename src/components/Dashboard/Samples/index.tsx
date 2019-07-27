add3dff8808a8cc26c94fe063f230250664f0f3d
iki789 <ikhlak789@gmail.com>
2019-07-27 10:11:13 +0530

    added placeholder select patient to view sample

--- src/components/Dashboard/Samples/index.tsx
+++ src/components/Dashboard/Samples/index.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { IStore, IRootState } from '../../../store';
 import {
 	createStyles,
 	makeStyles,
 	Paper,
+	Grid,
 	Table,
 	TableBody,
 	TableRow,
@@ -19,18 +22,23 @@ import { Sample } from '../../../lib/sample';
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
 		paper: {
-			padding: theme.spacing(2)
+			padding: theme.spacing(2),
+			height: 400
 		},
 		table: {
 			overflow: 'hidden',
 			overflowX: 'scroll',
 			marginLeft: -theme.spacing(2),
-			marginRight: -theme.spacing(2)
+			marginRight: -theme.spacing(2),
+			height: 348
 		},
 		hover: {
 			'&:hover': {
 				cursor: 'pointer'
 			}
+		},
+		placeholder: {
+			color: theme.palette.grey[300]
 		},
 		selected: {
 			backgroundColor: `${theme.palette.primary.light} !important`
@@ -38,17 +46,29 @@ const useStyles = makeStyles((theme: Theme) =>
 	})
 );
 
-export const Samples: React.FC<PatientProps> = (props: PatientProps) => {
+const Samples: React.FC<PatientProps> = (props: PatientProps) => {
 	const classes = useStyles();
 	const sampleService: Sample = new Sample();
 	sampleService.setPerPage = 6;
-	const [ samples, setSamples ] = useState(sampleService.getByPatient(1, 1));
+	const [ samples, setSamples ] = useState(sampleService.getByPatient(props.state.selectedPatient, 1));
 	const [ selectedSample, setSelectedSample ] = useState();
 	const [ pageOffset, setPageOffset ] = useState(0);
+	const [ currentPage, setCurrentPage ] = useState(1);
+	const [ hasData, setHasData ] = useState(false);
+
+	useEffect(
+		() => {
+			let patients = sampleService.getByPatient(props.state.selectedPatient, currentPage);
+			setSamples(patients);
+			setHasData(patients.length > 0);
+		},
+		[ hasData, currentPage, sampleService, props.state.selectedPatient ]
+	);
 
 	const handlePageChange = (e: React.MouseEvent<HTMLElement>, offset: number, page: number) => {
+		setCurrentPage(page);
 		setPageOffset(offset);
-		setSamples(sampleService.getByPatient(1, page));
+		setSamples(sampleService.getByPatient(props.state.selectedPatient, page));
 	};
 
 	const handleRowClick = (e: React.MouseEvent<HTMLElement>, id: number) => {
@@ -56,8 +76,8 @@ export const Samples: React.FC<PatientProps> = (props: PatientProps) => {
 		props.onSelect(id);
 	};
 
-	return (
-		<Paper className={classes.paper}>
+	const dataTable = (
+		<div>
 			<Typography variant="h5">Patient Samples</Typography>
 			<div className={classes.table}>
 				<Table>
@@ -92,10 +112,33 @@ export const Samples: React.FC<PatientProps> = (props: PatientProps) => {
 				currentPageColor="primary"
 				onClick={handlePageChange}
 			/>
-		</Paper>
+		</div>
 	);
+
+	const placeholder = (
+		<Grid container alignContent="center" style={{ height: '100%' }}>
+			<Grid item>
+				<Typography variant="h5" className={classes.placeholder}>
+					{props.state.selectedPatient ? 'Patient has not samples' : 'Select patient to view samples'}
+				</Typography>
+			</Grid>
+		</Grid>
+	);
+
+	return <Paper className={classes.paper}>{hasData ? dataTable : placeholder}</Paper>;
 };
 
 interface PatientProps {
 	onSelect: (id: number) => void;
+	state: IRootState;
 }
+
+const mapStateToProps = (state: IStore) => {
+	return {
+		state: state.rootReducer
+	};
+};
+
+const connected = connect(mapStateToProps)(Samples);
+
+export { connected as Samples };
