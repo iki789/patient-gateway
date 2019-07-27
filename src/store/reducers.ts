e7d6932c18845017177fe757a0e2e1224dce0264
iki789 <ikhlak789@gmail.com>
2019-07-27 06:03:56 +0530

    added actions in reducers

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,8 +1,9 @@
 import { combineReducers, Reducer } from 'redux';
 import { IRootState } from './';
+import { actions } from './actions';
 import { Auth } from '../lib/auth';
 
-export const state: IRootState = {
+export let state: IRootState = {
 	userId: Auth.isLoggedIn && Auth.user ? Auth.user.id : null
 };
 
@@ -10,6 +11,13 @@ export const appReducer: Reducer = (
 	rootStore: IRootState = state,
 	action: { type: string; payload?: any }
 ): IRootState => {
+	switch (action.type) {
+		case actions.SELECTED_PATIENT:
+			rootStore = {
+				...rootStore,
+				selectedPatient: action.payload.patientId
+			};
+	}
 	return rootStore;
 };
 
