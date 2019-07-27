9fe9db7e8cd8c61e2b2c626e9db655e70d05fa49
iki789 <ikhlak789@gmail.com>
2019-07-27 20:00:11 +0530

    removed change handles and updated grid

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -4,6 +4,7 @@ import { Container, Grid, AppBar, Toolbar, Typography, withStyles } from '@mater
 import { Auth } from '../../lib/auth';
 import { Patients } from './Patients';
 import { Samples } from './Samples';
+import { Variants } from './Variants';
 
 class Dashboard extends Component<IDashboard & RouteComponentProps> {
 	componentDidMount() {
@@ -29,12 +30,15 @@ class Dashboard extends Component<IDashboard & RouteComponentProps> {
 					<Typography display="inline" variant="h6">
 						Dr. {Auth && Auth.user ? Auth.user.name : ''}
 					</Typography>
-					<Grid container spacing={4}>
-						<Grid item xs={12} sm={4}>
-							<Patients onSelect={this.hanldePatientSelect} />
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							<Patients />
 						</Grid>
-						<Grid item xs={12} sm={4}>
-							<Samples onSelect={this.hanldePatientSelect} />
+						<Grid item xs={12} md={4}>
+							<Samples />
+						</Grid>
+						<Grid item xs={12} md={8}>
+							<Variants onSelect={this.hanldePatientSelect} />
 						</Grid>
 					</Grid>
 				</Container>
