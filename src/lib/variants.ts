9bf9c35042a1866d45b2bd4e432ba045f51bcf61
iki789 <ikhlak789@gmail.com>
2019-07-28 20:11:27 +0530

    implemented sort compatibilty for getBySampleId

--- src/lib/variants.ts
+++ src/lib/variants.ts
@@ -19,11 +19,46 @@ export class Variant extends Pagination implements IBase {
 		return this.getItemsOnPage(page);
 	}
 
-	getBySampleId(patientId: number, page: number) {
+	getBySampleId(patientId: number, page: number, sort?: sort) {
 		this.items = this.variants.filter((v) => v.sampleId === patientId);
+		if (sort) {
+			if (sort.field === 'mutationType') {
+				this.items.sort((a: IVariant, b: IVariant) => {
+					let mutationSeverity: { a: number; b: number } = {
+						a: this.getMutationSeverity(a.mutationType),
+						b: this.getMutationSeverity(b.mutationType)
+					};
+					return (
+						mutationSeverity[sort.direction === 'asc' ? 'a' : 'b'] -
+						mutationSeverity[sort.direction === 'asc' ? 'b' : 'a']
+					);
+				});
+			} else if (sort.field === 'alleleFrequency') {
+				this.items.sort(
+					(a: IVariant, b: IVariant) =>
+						sort.direction === 'asc' ? a.alleleFrequency - b.alleleFrequency : b.alleleFrequency - a.alleleFrequency
+				);
+			}
+		}
 		return this.getItemsOnPage(page);
 	}
+
+	private getMutationSeverity(mutationType: string): number {
+		switch (mutationType) {
+			case 'moderate_risk':
+				return 1;
+			case 'severe':
+				return 2;
+			default:
+				return 0;
+		}
+	}
 }
+
+type sort = {
+	field: 'mutationType' | 'alleleFrequency';
+	direction: 'asc' | 'desc';
+};
 
 interface IBase {
 	getById: (id: number) => IVariant;
