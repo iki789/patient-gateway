db0829d02220dcc111a451c035244b11027e0e2d
iki789 <ikhlak789@gmail.com>
2019-07-29 04:49:50 +0530

    added change handles for calendar

--- src/components/Dashboard/Samples/calendarPopover.tsx
+++ src/components/Dashboard/Samples/calendarPopover.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Button, Grid, makeStyles, Theme } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Button, Grid, Zoom, makeStyles, Theme } from '@material-ui/core';
 import { MuiPickersUtilsProvider, KeyboardDatePicker, MaterialUiPickersDate } from '@material-ui/pickers';
 import moment from '@date-io/moment';
+import momentJs from 'moment';
 
 const useStyles = makeStyles((theme: Theme) => {
 	return {
@@ -15,50 +16,65 @@ const useStyles = makeStyles((theme: Theme) => {
 			margin: -theme.spacing(2),
 			padding: theme.spacing(2),
 			color: theme.palette.common.white,
-			display: 'flex'
+			display: 'flex',
+			zIndex: 1
 		}
 	};
 });
 
 export const CalendarPopover = (props: CalendarPopoverProps) => {
 	const classes = useStyles();
+	const [ dates, setDates ] = useState<{ from: Date; to: Date }>({
+		from: momentJs().subtract(6, 'months').toDate(),
+		to: momentJs().toDate()
+	});
 
 	const handleFromDateChange = (e: MaterialUiPickersDate) => {
-		console.log(e);
+		if (e) {
+			setDates({ ...dates, from: e.toDate() });
+		}
+	};
+
+	const handleToDateChange = (e: MaterialUiPickersDate) => {
+		if (e) {
+			setDates({ ...dates, to: e.toDate() });
+		}
 	};
 
 	return (
 		<MuiPickersUtilsProvider utils={moment}>
-			<div className={classes.root}>
-				<Grid container spacing={2} alignContent="center" style={{ textAlign: 'center' }}>
-					<Grid item xs={12}>
-						<KeyboardDatePicker
-							disableFuture
-							openTo="year"
-							format="DD MMM YYYY"
-							label="From"
-							views={[ 'year', 'month', 'date' ]}
-							value={new Date()}
-							onChange={handleFromDateChange}
-						/>
+			<Zoom in={true}>
+				<div className={classes.root}>
+					<Grid container spacing={2} alignContent="center" style={{ textAlign: 'center' }}>
+						<Grid item xs={12}>
+							<KeyboardDatePicker
+								disableFuture
+								openTo="year"
+								format="DD MMM YYYY"
+								label="From"
+								views={[ 'year', 'month', 'date' ]}
+								value={dates.from}
+								onChange={handleFromDateChange}
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<KeyboardDatePicker
+								disableFuture
+								openTo="year"
+								format="DD MMM YYYY"
+								label="To"
+								views={[ 'year', 'month', 'date' ]}
+								value={dates.to}
+								onChange={handleToDateChange}
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<Button variant="text">Clear</Button>
+							<Button variant="text">OK</Button>
+						</Grid>
 					</Grid>
-					<Grid item xs={12}>
-						<KeyboardDatePicker
-							disableFuture
-							openTo="year"
-							format="DD MMM YYYY"
-							label="To"
-							views={[ 'year', 'month', 'date' ]}
-							value={new Date()}
-							onChange={handleFromDateChange}
-						/>
-					</Grid>
-					<Grid item xs={12}>
-						<Button variant="text">Clear</Button>
-						<Button variant="text">OK</Button>
-					</Grid>
-				</Grid>
-			</div>
+				</div>
+			</Zoom>
 		</MuiPickersUtilsProvider>
 	);
 };
